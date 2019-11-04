d3222d8646ffa46594ca325253835fdc27cca813
Matvey Smychkov <matveysmychkov@Matveys-MacBook-Pro.local>
2019-11-04 13:56:12 -0800

    Added background

--- init.sh
+++ init.sh
@@ -8,16 +8,13 @@ brew cask install iterm2
 git clone https://github.com/MartinSeeler/iterm2-material-design
 
 # keyboard layouts 
-sudo cp mac/typography-layout/English - IBT.keylayout /Library/Keyboard\ Layouts/
-sudo cp mac/typography-layout/English - IBT.icns /Library/Keyboard\ Layouts/
-sudo cp mac/typography-layout/Russian - IBT.keylayout /Library/Keyboard\ Layouts/
-sudo cp mac/typography-layout/Russian - IBT.icns /Library/Keyboard\ Layouts/
+sudo cp "mac/typography-layout/English - IBT.keylayout" "/Library/Keyboard Layouts/"
+sudo cp "mac/typography-layout/English - IBT.icns" "/Library/Keyboard Layouts/"
+sudo cp "mac/typography-layout/Russian - IBT.keylayout" "/Library/Keyboard Layouts/"
+sudo cp "mac/typography-layout/Russian - IBT.icns" "/Library/Keyboard Layouts/"
 
 # httpie
 brew install httpie
-
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -25,8 +22,12 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # java
 brew tap AdoptOpenJDK/openjdk
 brew cask install adoptopenjdk13
+brew cask install adoptopenjdk11
 brew cask install adoptopenjdk8
 brew install gradle
 
 # vim
 cp vim/.vimrc ~/
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
